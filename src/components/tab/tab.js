f5411d16cb3af18ba7a867a35eeba76e5ec546ef
alexander <alexander.silverman25@gmail.com>
2018-12-03 02:17:12 +0200

    limit customize Headers only for extension pages

--- src/components/tab/tab.js
+++ src/components/tab/tab.js
@@ -55,15 +55,17 @@ class Tab extends Component {
 
     allowIframeOrigin() {
         ChromeHelper.instance().webRequest.onHeadersReceived.addListener(
-            function (info) {
-                var headers = info.responseHeaders;
-                for (var i = headers.length - 1; i >= 0; --i) {
-                    var header = headers[i].name.toLowerCase();
-                    if (header == 'x-frame-options' || header == 'frame-options') {
-                        headers.splice(i, 1); // Remove header
+            (info) => {
+                if(info.url == this.state.iframeUrl) {
+                    var headers = info.responseHeaders;
+                    for (var i = headers.length - 1; i >= 0; --i) {
+                        var header = headers[i].name.toLowerCase();
+                        if (header == 'x-frame-options' || header == 'frame-options') {
+                            headers.splice(i, 1); // Remove header
+                        }
                     }
+                    return {responseHeaders: headers};
                 }
-                return {responseHeaders: headers};
             },
             {
                 urls: ['*://*/*'], // Pattern to match all http(s) pages
